4d9a482148041468636fd1c188513a7365cc32d2
4yH4yMaPy <144848144+4yH4yMaPy@users.noreply.github.com>
2024-01-24 18:39:36 +0300

    Add files via upload

---
+++ рандом/рандом/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int rn = r.Next(0, 101);
+
+            while (true)
+            {
+                string t = Console.ReadLine();
+                int mn = Convert.ToInt32(t);
+
+                if (rn > mn)
+                {
+                    Console.WriteLine("Загаданное число больше.");
+                }
+                else if (rn < mn)
+                {
+                    Console.WriteLine("Загаданное число меньше.");
+                }
+                else if (rn == mn)
+                {
+                    Console.WriteLine("Вы выиграли!");
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
